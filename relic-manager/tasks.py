e36ecf1b497de80669a60308c44971a08cead788
shdewz <shdewz@gmail.com>
2025-04-01 17:00:49 +0300

    Add OS-dependent switch for pty

--- relic-manager/tasks.py
+++ relic-manager/tasks.py
@@ -1,14 +1,24 @@
 from invoke import task
+import sys
+pty_enable = False if sys.platform=="win32" else True
 
 
 @task
 def start(ctx):
-    ctx.run("python src/index.py")
+    ctx.run("python src/index.py", pty=pty_enable)
 
 @task
 def build(ctx):
-    ctx.run("python src/build.py")
+    ctx.run("python src/build.py", pty=pty_enable)
 
 @task
 def test(ctx):
-    ctx.run("pytest src", pty=True)
+    ctx.run("pytest src", pty=pty_enable)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=pty_enable)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=pty_enable)
