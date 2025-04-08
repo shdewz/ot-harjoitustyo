19deb3d617108fc5f4120959f513d102d506e0b3
shdewz <shdewz@gmail.com>
2025-04-08 16:36:13 +0300

    Implement IDs in testing

--- relic-manager/src/tests/services/relic_service_test.py
+++ relic-manager/src/tests/services/relic_service_test.py
@@ -6,11 +6,14 @@ from entities.relic import Relic
 class FakeRelicRepository:
     def __init__(self, relics=None):
         self.relics = relics or []
+        self.counter = 0
 
     def find_all(self):
         return self.relics
     
     def create(self, relic):
+        self.counter += 1
+        relic.id = self.counter
         self.relics.append(relic)
         return relic
     
@@ -25,7 +28,7 @@ class TestRelicService(unittest.TestCase):
 
 
     def test_create_relic(self):
-        self.relic_service.create(1, "Example Set", "Head", 15, "HP", [("SPD", 6.2), ("ATK%", 8.7), ("EHR%", 11.9), ("ATK", 76)])
+        self.relic_service.create("Example Set", "Head", 15, "HP", [("SPD", 6.2), ("ATK%", 8.7), ("EHR%", 11.9), ("ATK", 76)])
         relics = self.relic_service.get_all()
 
         self.assertEqual(len(relics), 1)
@@ -34,11 +37,24 @@ class TestRelicService(unittest.TestCase):
         self.assertAlmostEqual(int(relics[0].mainstat_value), 705)
 
     def test_delete_relic(self):
-        self.relic_service.create(1, "Example Set", "Head", 15, "HP", [("SPD", 6.2), ("ATK%", 8.7), ("EHR%", 11.9), ("ATK", 76)])
+        self.relic_service.create("Example Set", "Head", 15, "HP", [("SPD", 6.2), ("ATK%", 8.7), ("EHR%", 11.9), ("ATK", 76)])
         relics = self.relic_service.get_all()
         self.assertEqual(len(relics), 1)
 
         self.relic_service.delete(1)
         relics = self.relic_service.get_all()
         self.assertEqual(len(relics), 0)
+
+    def test_delete_one_of_many(self):
+        self.relic_service.create("Example Set", "Head", 15, "HP", [("SPD", 6.2), ("ATK%", 8.7), ("EHR%", 11.9), ("ATK", 76)])
+        self.relic_service.create("Example Set", "Body", 15, "CRATE%", [("SPD", 6.2), ("ATK%", 8.7), ("EHR%", 11.9), ("ATK", 76)])
+        relics = self.relic_service.get_all()
+        self.assertEqual(len(relics), 2)
+
+        self.relic_service.delete(1)
+        relics = self.relic_service.get_all()
+        self.assertEqual(len(relics), 1)
+        self.assertEqual(relics[0].relic_type, "Body")
+        self.assertAlmostEqual(round(relics[0].mainstat_value, 1), 32.4)
+
         
