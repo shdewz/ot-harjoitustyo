f2bc7d3512e68cf71efeaccc48a1fb2fda4c9442
shdewz <shdewz@gmail.com>
2025-04-08 15:50:12 +0300

    Include all 4 substats

--- relic-manager/src/create_sample_db.py
+++ relic-manager/src/create_sample_db.py
@@ -9,13 +9,35 @@ if __name__ == "__main__":
     cursor = connection.cursor()
 
     cursor.execute("""
-        insert into relics (relic_set, type, level, mainstat, substat1, substat1value)
-        values ('Scholar Lost in Erudition', 'Head', 15, 'ATK%', 'SPD', 7.6)
+        insert into relics (relic_set, type, level, mainstat,
+                            substat1, substat1value,
+                            substat2, substat2value,
+                            substat3, substat3value,
+                            substat4, substat4value
+        )
+        values ('Scholar Lost in Erudition',
+                'Head', 15, 'ATK',
+                'SPD', 7.6,
+                'ATK', 81,
+                'HP', 65,
+                'EHR%', 11.4
+        )
     """)
 
     cursor.execute("""
-        insert into relics (relic_set, type, level, mainstat, substat1, substat1value)
-        values ('Scholar Lost in Erudition', 'Body', 15, 'CDMG', 'SPD', 7.1)
+        insert into relics (relic_set, type, level, mainstat,
+                            substat1, substat1value,
+                            substat2, substat2value,
+                            substat3, substat3value,
+                            substat4, substat4value
+        )
+        values ('Scholar Lost in Erudition',
+                'Body', 15, 'CDMG%',
+                'SPD', 7.1,
+                'ATK', 71,
+                'HP', 75,
+                'EHR%', 10.4
+        )
     """)
 
     connection.commit()
