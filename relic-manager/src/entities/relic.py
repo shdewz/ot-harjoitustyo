ad15dd32a32d22d89845fb000e8f43338274d12b
shdewz <shdewz@gmail.com>
2025-04-15 17:57:43 +0300

    Allow proper relic input

--- relic-manager/src/entities/relic.py
+++ relic-manager/src/entities/relic.py
@@ -1,34 +1,92 @@
 relic_mainstats = {
-    # (initial, per-level)
+    # (full name, initial, per-level)
     # from https://honkai-star-rail.fandom.com/wiki/Relic/Stats#Main_Stat_Values
-    "SPD": (4.032, 1.4),
-    "HP": (112.896, 39.5136),
-    "HP%": (6.912, 2.4192),
-    "ATK": (56.448, 19.7568),
-    "ATK%": (6.912, 2.4192),
-    "DEF%": (8.64, 3.024),
-    "BE%": (10.368, 3.6277),
-    "EHR%": (6.912, 2.4192),
-    "ERR%": (3.1104, 1.0886),
-    "OHB%": (5.5296, 1.9354),
-    "ELEM%": (6.2208, 2.1773),
-    "CRATE%": (5.184, 1.8144),
-    "CDMG%": (10.368, 3.6288),
+    "SPD": ("Speed", 4.032, 1.4),
+    "HP": ("HP", 112.896, 39.5136),
+    "HP%": ("HP%", 6.912, 2.4192),
+    "ATK": ("ATK", 56.448, 19.7568),
+    "ATK%": ("ATK%", 6.912, 2.4192),
+    "DEF%": ("DEF%", 8.64, 3.024),
+    "BE%": ("Break Effect", 10.368, 3.6277),
+    "EHR%": ("Effect Hit Rate", 6.912, 2.4192),
+    "ERR%": ("Energy Recharge Rate", 3.1104, 1.0886),
+    "OHB%": ("Overall Healing Bonus", 5.5296, 1.9354),
+    "ELEM%": ("Elemental Damage", 6.2208, 2.1773),
+    "CRATE%": ("Crit Rate", 5.184, 1.8144),
+    "CDMG%": ("Crit Damage", 10.368, 3.6288),
 }
 
 relic_substats = {
-    # (low, mid, high) rolls of substats; 1 roll per 3 levels
-    "SPD": (2, 2.3, 2.6),
-    "HP": (33.87, 38.103755, 42.33751),
-    "HP%": (3.456, 3.888, 4.32),
-    "ATK": (16.935, 19.051877, 21.168754),
-    "ATK%": (3.456, 3.888, 4.32),
-    "DEF%": (4.32, 4.86, 5.4),
-    "BE%": (5.184, 5.832, 6.48),
-    "EHR%": (3.456, 3.888, 4.32),
-    "RES%": (3.456, 3.888, 4.32),
-    "CRATE%": (2.592, 2.916, 3.24),
-    "CDMG%": (5.184, 5.832, 6.48),
+    # (full name, low, mid, high) rolls of substats; 1 roll per 3 levels
+    "SPD": ("Speed", 2, 2.3, 2.6),
+    "HP": ("HP", 33.87, 38.103755, 42.33751),
+    "HP%": ("HP%", 3.456, 3.888, 4.32),
+    "ATK": ("ATK", 16.935, 19.051877, 21.168754),
+    "ATK%": ("ATK%", 3.456, 3.888, 4.32),
+    "DEF%": ("DEF%", 4.32, 4.86, 5.4),
+    "BE%": ("Break Effect", 5.184, 5.832, 6.48),
+    "EHR%": ("Effect Hit Rate", 3.456, 3.888, 4.32),
+    "RES%": ("Effect RES", 3.456, 3.888, 4.32),
+    "CRATE%": ("Crit Rate", 2.592, 2.916, 3.24),
+    "CDMG%": ("Crit Damage", 5.184, 5.832, 6.48),
+}
+
+relic_types = {
+    # 0 = standard, 1 = planar; for set separation
+    "Head": 0,
+    "Hands": 0,
+    "Body": 0,
+    "Feet": 0,
+    "Ball": 1,
+    "Rope": 1
+}
+
+relic_sets = {
+    # 0 = standard, 1 = planar; for type separation
+    "Band of Sizzling Thunder": 0,
+    "Champion of Streetwise Boxing": 0,
+    "Eagle of Twilight Line": 0,
+    "Firesmith of Lava-Forging": 0,
+    "Genius of Brilliant Stars": 0,
+    "Guard of Wuthering Snow": 0,
+    "Hero of Triumphant Song": 0,
+    "Hunter of Glacial Forest": 0,
+    "Iron Cavalry Against the Scourge": 0,
+    "Knight of Purity Palace": 0,
+    "Longevous Disciple": 0,
+    "Messenger Traversing Hackerspace": 0,
+    "Musketeer of Wild Wheat": 0,
+    "Passerby of Wandering Cloud": 0,
+    "Pioneer Diver of Dead Waters": 0,
+    "Poet of Mourning Collapse": 0,
+    "Prisoner in Deep Confinement": 0,
+    "Sacerdos' Relived Ordeal": 0,
+    "Scholar Lost in Erudition": 0,
+    "The Ashblazing Grand Duke": 0,
+    "The Wind-Soaring Valorous": 0,
+    "Thief of Shooting Meteor": 0,
+    "Wastelander of Banditry Desert": 0,
+    "Watchmaker, Master of Dream Machinations": 0,
+    "Belobog of the Architects": 1,
+    "Bone Collection's Serene Demesne": 1,
+    "Broken Keel": 1,
+    "Celestial Differentiator": 1,
+    "Duran, Dynasty of Running Wolves": 1,
+    "Firmament Frontline: Glamoth": 1,
+    "Fleet of the Ageless": 1,
+    "Forge of the Kalpagni Lantern": 1,
+    "Giant Tree of Rapt Brooding": 1,
+    "Inert Salsotto": 1,
+    "Izumo Gensei and Takama Divine Realm": 1,
+    "Lushaka, the Sunken Seas": 1,
+    "Pan-Cosmic Commercial Enterprise": 1,
+    "Penacony, Land of the Dreams": 1,
+    "Rutilant Arena": 1,
+    "Sigonia, the Unclaimed Desolation": 1,
+    "Space Sealing Station": 1,
+    "Sprightly Vonwacq": 1,
+    "Talia: Kingdom of Banditry": 1,
+    "The Wondrous BananAmusement Park": 1
 }
 
 class Relic:
@@ -54,5 +112,6 @@ class Relic:
         self.level = level
         self.mainstat = mainstat
         mainstat_stats = relic_mainstats[self.mainstat]
-        self.mainstat_value = mainstat_stats[0] + self.level * mainstat_stats[1]
+        print(mainstat_stats)
+        self.mainstat_value = mainstat_stats[1] + self.level * mainstat_stats[2]
         self.substats = substats
--- relic-manager/src/ui/add_relic_view.py
+++ relic-manager/src/ui/add_relic_view.py
@@ -1,12 +1,14 @@
 from tkinter import ttk, constants, StringVar
 from services.relic_service import relic_service
-from entities.relic import Relic
+from entities.relic import Relic, relic_mainstats, relic_substats, relic_types, relic_sets
 
 class AddRelicView:
     def __init__(self, root, handle_show_relics_view):
         self._root = root
         self._frame = None
         self._handle_show_relics_view = handle_show_relics_view
+        self._substat_fields = {}
+        self._relic_group = None
 
         self._initialize()
 
@@ -16,57 +18,83 @@ class AddRelicView:
     def destroy(self):
         self._frame.destroy()
 
+    def _add_substat_entry(self, index, row_index):
+        row_index = row_index + (index - 1)
+        self._substat_fields[f"sub{index}_label"] = ttk.Label(master=self._frame, text=f"Substat {index}:")
+        self._substat_fields[f"sub{index}_label"].grid(row=row_index, column=0, padx=5, pady=5, sticky=constants.E)
+
+        self._substat_fields[f"sub{index}_entry_type_var"] = StringVar()
+        self._substat_fields[f"sub{index}_entry_type"] = ttk.Combobox(master=self._frame,
+                                                                      textvariable=self._substat_fields[f"sub{index}_entry_type_var"],
+                                                                      values=list(map(lambda x: x[0], relic_substats.values()))
+                                                                      )
+        self._substat_fields[f"sub{index}_entry_type"].grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
+
+        self._substat_fields[f"sub{index}_entry_val"] = ttk.Entry(master=self._frame)
+        self._substat_fields[f"sub{index}_entry_val"].grid(row=row_index, column=2, padx=5, pady=5, sticky=constants.EW)
+
+    def _add_relic(self):
+        _relic_set = self._set_entry.get()
+        _relic_type = self._type_entry.get()
+        _relic_level = int(self._level_entry.get())
+        _relic_mainstat_full = self._mainstat_entry.get()
+        _relic_mainstat = {v[0]: k for k, v in relic_mainstats.items()}.get(_relic_mainstat_full)
+        _relic_substats = []
+        for i in range(4):
+            substat_type_full = self._substat_fields[f"sub{i + 1}_entry_type"].get()
+            substat = {v[0]: k for k, v in relic_substats.items()}.get(substat_type_full)
+            substat_value = float(self._substat_fields[f"sub{i + 1}_entry_val"].get())
+            _relic_substats.append((substat, substat_value))
+
+        relic_service.create(_relic_set, _relic_type, _relic_level, _relic_mainstat, _relic_substats)
+        self._handle_show_relics_view()
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
         self._relic_list_frame = ttk.Frame(master=self._frame)
 
-        title_label = ttk.Label(
-            master=self._frame,
-            text="Add new relic"
-        )
+        title_label = ttk.Label(master=self._frame, text="Add new relic")
 
         title_label.grid(row=0, column=0, padx=5, pady=5, sticky=constants.EW)
-
-        # current_var = StringVar()
-        # combobox = ttk.Combobox(master=self._frame, textvariable=current_var, values=["1", "2", "3"])
-
-        # combobox.grid(
-        #     row=1,
-        #     column=1,
-        #     padx=5,
-        #     pady=5,
-        #     sticky=constants.EW
-        # )
 
         row_index = 1
 
         set_label = ttk.Label(master=self._frame, text="Relic Set:")
         set_label.grid(row=row_index, column=0, padx=5, pady=5, sticky=constants.E)
         set_var = StringVar()
-        set_entry = ttk.Combobox(master=self._frame, textvariable=set_var, values=["1", "2", "3"])
-        set_entry.grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
+        self._set_entry = ttk.Combobox(master=self._frame, textvariable=set_var, values=list(relic_sets.keys()))
+        self._set_entry.grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
         row_index += 1
 
         type_label = ttk.Label(master=self._frame, text="Type:")
         type_label.grid(row=row_index, column=0, padx=5, pady=5, sticky=constants.E)
         type_var = StringVar()
-        type_entry = ttk.Combobox(master=self._frame, textvariable=type_var, values=["Head", "Hands", "Body", "Feet"])
-        type_entry.grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
+        self._type_entry = ttk.Combobox(master=self._frame, textvariable=type_var, values=list(relic_types.keys()))
+        self._type_entry.grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
         row_index += 1
 
         level_label = ttk.Label(master=self._frame, text="Level:")
         level_label.grid(row=row_index, column=0, padx=5, pady=5, sticky=constants.E)
-        level_entry = ttk.Entry(master=self._frame)
-        level_entry.grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
+        self._level_entry = ttk.Entry(master=self._frame)
+        self._level_entry.grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
         row_index += 1
 
-        sub1_label = ttk.Label(master=self._frame, text="Substat 1:")
-        sub1_label.grid(row=row_index, column=0, padx=5, pady=5, sticky=constants.E)
-        sub1_entry = ttk.Entry(master=self._frame)
-        sub1_entry.grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
-        sub1_entry_val = ttk.Entry(master=self._frame)
-        sub1_entry_val.grid(row=row_index, column=2, padx=5, pady=5, sticky=constants.EW)
+        mainstat_label = ttk.Label(master=self._frame, text="Mainstat:")
+        mainstat_label.grid(row=row_index, column=0, padx=5, pady=5, sticky=constants.E)
+        mainstat_var = StringVar()
+        self._mainstat_entry = ttk.Combobox(master=self._frame, textvariable=mainstat_var, values=list(map(lambda x: x[0], relic_mainstats.values())))
+        self._mainstat_entry.grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
+
+        sub_val_label = ttk.Label(master=self._frame, text="Substat values:")
+        sub_val_label.grid(row=row_index, column=2, padx=5, pady=5, sticky=constants.EW)
         row_index += 1
 
-        add_relic_button = ttk.Button(master=self._frame, text="Confirm", command=self._handle_show_relics_view)
-        add_relic_button.grid(row=row_index, column=1, padx=5, pady=5, sticky=constants.EW)
+        self._add_substat_entry(1, row_index)
+        self._add_substat_entry(2, row_index)
+        self._add_substat_entry(3, row_index)
+        self._add_substat_entry(4, row_index)
+        row_index += 4
+
+        add_relic_button = ttk.Button(master=self._frame, text="Confirm", command=self._add_relic)
+        add_relic_button.grid(row=row_index, column=2, padx=5, pady=5, sticky=constants.EW)
+
