a082d27ebbc33be6d2cf86e7a06372db12920e6e
shdewz <shdewz@gmail.com>
2025-04-29 15:32:27 +0300

    Add function to delete relics

--- relic-manager/src/ui/relics_view.py
+++ relic-manager/src/ui/relics_view.py
@@ -1,6 +1,7 @@
 from tkinter import ttk, constants
 from services.relic_service import relic_service
 from entities.relic import Relic
+from tkinter.messagebox import askyesno
 
 class RelicsView:
     def __init__(self, root, handle_show_add_relic_view):
@@ -43,9 +44,15 @@ class RelicsView:
         relics = relic_service.get_all()
         self._initialize_relic_table(relics)
 
+    def _delete_relic(self, relic_id):
+        relic_service.delete(relic_id)
+        relics = relic_service.get_all()
+        self._initialize_relic_table(relics)
+
     def _initialize_relic_table(self, relics: list[Relic]):
         columns = [
             ("x", 20, "center"),
+            ("ID", 20, "center"),
             ("Set", 200, "w"),
             ("Type", 50, "center"),
             ("Level", 40, "center"),
@@ -69,6 +76,7 @@ class RelicsView:
 
             tree.insert("", "end", values=[
                 "x",
+                relic.relic_id,
                 relic.relic_set,
                 relic.relic_type,
                 relic.level,
@@ -84,6 +92,8 @@ class RelicsView:
             sticky=constants.EW
         )
 
+        tree.bind("<Double-1>", lambda event: self.on_treeview_click(event, tree))
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
         self._relic_list_frame = ttk.Frame(master=self._frame)
@@ -98,9 +108,28 @@ class RelicsView:
             columnspan=2,
             sticky=constants.EW
         )
+        
+        footer_label = ttk.Label(
+            master=self._frame,
+            text="Double-click 'x' to remove"
+        )
+
+        footer_label.grid(row=2, column=0, padx=5, pady=5, sticky=constants.W)
 
         self._frame.grid_columnconfigure(0, weight=1)
         self._frame.grid_columnconfigure(1, weight=0)
+
+    def on_treeview_click(self, event, tree):
+        region = tree.identify("region", event.x, event.y)
+        if region == "cell":
+            item = tree.identify_row(event.y)
+            column = tree.identify_column(event.x)
+            relic_id = tree.set(item, '#2')
+            cell_value = tree.set(item, column)
+            if cell_value == "x":
+                answer = askyesno(title="Confirm deletion", message="Are you sure you want to delete the relic?")
+                if answer:
+                    self._delete_relic(relic_id)
 
 def format_stat(stat, value):
     percent = "%" if stat[-1] == "%" else ""
@@ -113,3 +142,4 @@ def sort(tree, col, desc):
     for index, (val, item) in enumerate(data):
         tree.move(item, "", index)
     tree.heading(col, command=lambda: sort(tree, col, not desc))
+
